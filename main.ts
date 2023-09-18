8c72afe2d41ff28323034498685066917e91e355
letmepickausername <145314048+letmepickausername@users.noreply.github.com>
2023-09-18 14:32:07 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+music.play(music.stringPlayable("D C C5 C D C C5 C ", 2000), music.PlaybackMode.InBackground)
 basic.showNumber(1)
 basic.showLeds(`
     . . # . .
@@ -23,7 +24,6 @@ basic.showLeds(`
     . . # . .
     `)
 basic.forever(function () {
-    music.play(music.createSoundExpression(WaveShape.Triangle, 404, 5000, 18, 255, 5000, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
     music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
 })
